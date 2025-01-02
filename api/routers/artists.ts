eaba099d7de837efa1d474869140dbdefcd32455
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-02 21:16:36 +0600

    Created artists router, added get & post methods

---
+++ api/routers/artists.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import Artist from "../models/Artist";
+import {imagesUpload} from "../multer";
+import mongoose from "mongoose";
+import {IArtist} from "../types";
+
+const artistsRouter = express.Router();
+
+artistsRouter.get('/', async (req: express.Request, res: express.Response, next) => {
+    try {
+        const artists = await Artist.find();
+        res.send(artists);
+    } catch (e) {
+        next(e);
+    }
+});
+
+artistsRouter.post('/', imagesUpload.single('image'), async (req: express.Request, res: express.Response, next) => {
+    const {name, info} = req.body;
+
+    const newArtist: IArtist = {
+        name,
+        info,
+        image: req.file ? 'images' + req.file.filename : null,
+    };
+
+    try {
+        const artist = new Artist(newArtist);
+        await artist.save();
+        res.send(artist);
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            const ValidationErrors = Object.keys(error.errors).map((key: string) => (
+                    {
+                        field: key,
+                        message: error.errors[key].message,
+                    }
+                )
+            );
+            res.status(400).send({errors: ValidationErrors});
+        }
+        next(error);
+    }
+});
+
+export default artistsRouter;
