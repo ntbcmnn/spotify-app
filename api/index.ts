c90ee140c5ec04e05b5c65e157106daf0620baae
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-02 21:28:31 +0600

    Imported all routers

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import artistsRouter from "./routers/artists";
+import tracksRouter from "./routers/tracks";
+import albumsRouter from "./routers/albums";
 
 const app = express();
 const port = 8000;
@@ -8,6 +11,10 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/artists', artistsRouter);
+app.use('/albums', albumsRouter);
+app.use('/tracks', tracksRouter);
 
 const run: () => Promise<void> = async () => {
     await mongoose.connect('mongodb://localhost/spotify');
