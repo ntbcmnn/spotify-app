ce160fcd716610dcd5d6ce9d7f63e290335b3f9a
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-02 21:33:06 +0600

    Created interfaces for the better typification

---
+++ api/types.d.ts
@@ -0,0 +1,18 @@
+export interface IArtist {
+    name: string;
+    info: string;
+    image: string | null;
+}
+
+export interface IAlbum {
+    name: string;
+    artist: string;
+    release_year: number;
+    image: string | null;
+}
+
+export interface ITrack {
+    name: string;
+    artist: string;
+    duration: string;
+}
