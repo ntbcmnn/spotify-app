73308b6dc43b5876841e7e1cc254d60c289b00a0
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-03 13:58:01 +0600

    1. Created tracks router 2. Added validation (if query album id is invalid - server will send a response with status 400) 3. If no query parameters found - we receive all tracks from the db 4. If album id was in request - we receive all tracks related with this album

---
+++ api/routers/tracks.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import Track from "../models/Track";
+import {ITrack} from "../types";
+import mongoose from "mongoose";
+
+const tracksRouter = express.Router();
+
+tracksRouter.get('/', async (req: express.Request, res: express.Response, next) => {
+    const {album} = req.query;
+
+    try {
+        if (album) {
+            if (!mongoose.Types.ObjectId.isValid(album as string)) {
+                res.status(400).send('Invalid album ID');
+                return;
+            }
+
+            const tracks = await Track.find({album}).populate("album", "-_id name release_year");
+
+            if (tracks.length === 0) {
+                res.status(404).send('No tracks found for this album');
+                return;
+            }
+
+            res.send(tracks);
+        }
+
+        const tracks = await Track.find();
+        res.send(tracks);
+    } catch (e) {
+        next(e);
+    }
+});
+
+tracksRouter.post('/', async (req: express.Request, res: express.Response, next) => {
+    const {name, album, duration} = req.body;
+
+    const newTrack: ITrack = {
+        name,
+        album,
+        duration,
+    };
+
+    try {
+        const track = new Track(newTrack);
+        await track.save();
+        res.send(track);
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            const ValidationErrors = Object.keys(error.errors).map((key: string) => (
+                    {
+                        field: key,
+                        message: error.errors[key].message,
+                    }
+                )
+            );
+            res.status(400).send({errors: ValidationErrors});
+        }
+        next(error);
+    }
+});
+
+export default tracksRouter;
