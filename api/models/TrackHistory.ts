1e9731e0406eb54a00f97ac70cfb212cdf088093
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-08 09:22:23 +0600

    Created Track history schema, added all required fields and connections

---
+++ api/models/TrackHistory.ts
@@ -0,0 +1,21 @@
+import mongoose, {Schema} from "mongoose";
+
+const TrackHistorySchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User id is required'],
+    },
+    track: {
+        type: Schema.Types.ObjectId,
+        ref: 'Track',
+        required: [true, 'Track id is required'],
+    },
+    datetime: {
+        type: Date,
+        required: true,
+    }
+});
+
+const TrackHistory = mongoose.model('TrackHistory', TrackHistorySchema);
+export default TrackHistory;
