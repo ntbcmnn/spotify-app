171471fadec5654009dfec02bdbcf3df8f81e461
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-16 14:24:48 +0600

    1. Created Albums container 2. Used useParams to find the artist id and display the correct list of albums 3. Added a validation to get artist name from the albums array

---
+++ frontend/src/Containers/Albums/Albums.tsx
@@ -0,0 +1,41 @@
+import Loader from '../../Components/UI/Loader/Loader.tsx';
+import { IAlbum } from '../../types';
+import { useAppDispatch, useAppSelector } from '../../app/hooks.ts';
+import { selectAlbumsLoading, selectAllAlbums } from '../../store/slices/albumsSlice.ts';
+import Album from '../../Components/Album/Album.tsx';
+import { useEffect } from 'react';
+import { fetchAlbums } from '../../store/thunks/albumsThunk.ts';
+import { useParams } from 'react-router-dom';
+
+const Albums = () => {
+  const {artistId} = useParams();
+  const dispatch = useAppDispatch();
+  const albums: IAlbum[] = useAppSelector(selectAllAlbums);
+  const isLoading: boolean = useAppSelector(selectAlbumsLoading);
+
+  useEffect(() => {
+    if (artistId) {
+      dispatch(fetchAlbums(artistId));
+    }
+  }, [dispatch, artistId]);
+
+  return (
+    <>
+      {isLoading ? <Loader/> :
+        <>
+          <h1 className="text-center mb-5">{albums.length > 0 ? albums[0].artist.name : 'No albums found'}</h1>
+          <div className="container d-flex flex-wrap gap-4 justify-content-center">
+            {
+              albums.length > 0 ?
+                albums.map((album: IAlbum) => (
+                  <Album album={album} key={album._id}/>
+                )) : <h1>No albums found</h1>
+            }
+          </div>
+        </>
+      }
+    </>
+  );
+};
+
+export default Albums;
