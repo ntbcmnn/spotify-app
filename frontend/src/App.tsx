4e5d1d0b26ba0824d697b2967c51594a4ebbd9f7
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-16 14:27:05 +0600

    1. Added Toolbar 2. Added routes to the containers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Artists from './Containers/Artists/Artists.tsx';
+import Toolbar from './Components/UI/Toolbar/Toolbar.tsx';
+import Albums from './Containers/Albums/Albums.tsx';
+import Tracks from './Containers/Tracks/Tracks.tsx';
 
-const App = () => <></>;
+const App = () => <>
+  <Toolbar/>
+  <div className="container m-5">
+    <Routes>
+      <Route path="/" element={<Artists/>}/>
+      <Route path="/artists" element={<Artists/>}/>
+      <Route path="/albums/:artistId" element={<Albums />} />
+      <Route path="/tracks/:albumId" element={<Tracks />} />
+      <Route path="*" element={<h3 className="text-center">Page not found</h3>}/>
+    </Routes>
+  </div>
+</>;
 
 export default App;
