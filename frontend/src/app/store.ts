2f46f2b53f4dec09cae9dfcdd79879f575541c1d
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-16 14:19:44 +0600

    Imported artists, albums and tracks reducers

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { artistsReducer } from '../store/slices/artistsSlice.ts';
+import { albumsReducer } from '../store/slices/albumsSlice.ts';
+import { tracksReducer } from '../store/slices/tracksSlice.ts';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    artists: artistsReducer,
+    albums: albumsReducer,
+    tracks: tracksReducer
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
