75c3316454efda04291c38998537a901d31fef58
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-16 13:47:16 +0600

    Updated config.ts: removed unused fields, pointed the right path to our server

--- backend/config.ts
+++ backend/config.ts
@@ -1,17 +1,12 @@
 import path from "path";
 
-const rootPath: string = __dirname;
+
+const rootPath  = __dirname;
 
 const config = {
     rootPath,
     publicPath: path.join(rootPath, 'public'),
-    dataBase: {
-        host: 'localhost',
-        user: 'root',
-        password: '1qaz@WSX29',
-        database: 'spotify',
-    }
+    db: 'mongodb://localhost/spotify',
 };
-
 
 export default config;
