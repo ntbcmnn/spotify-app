4d267e702933b302317e0b4b83ba022438807701
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-20 02:39:29 +0600

    [HW#86] Updated store, added redux-persist methods

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,15 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { artistsReducer } from '../store/slices/artistsSlice.ts';
 import { albumsReducer } from '../store/slices/albumsSlice.ts';
 import { tracksReducer } from '../store/slices/tracksSlice.ts';
+import { usersReducer } from '../store/slices/usersSlice.ts';
+import { trackHistoryReducer } from '../store/slices/trackHistorySlice.ts';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
+
+const userPersistConfig = {
+  key: 'store:users',
+  storage,
+  whitelist: ['user']
+};
+
+const rootReducer = combineReducers({
+  artists: artistsReducer,
+  albums: albumsReducer,
+  tracks: tracksReducer,
+  history: trackHistoryReducer,
+  users: persistReducer(userPersistConfig, usersReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    artists: artistsReducer,
-    albums: albumsReducer,
-    tracks: tracksReducer
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
