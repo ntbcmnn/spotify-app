31fe3778248a7e83b5a2cfe62202b5d3940d5160
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-20 02:46:07 +0600

    [HW#86] Imported login, register and history components, created routes for them

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import Artists from './Containers/Artists/Artists.tsx';
 import Toolbar from './Components/UI/Toolbar/Toolbar.tsx';
 import Albums from './Containers/Albums/Albums.tsx';
 import Tracks from './Containers/Tracks/Tracks.tsx';
+import LoginPage from './Containers/LoginPage/LoginPage.tsx';
+import RegisterPage from './Containers/RegisterPage/RegisterPage.tsx';
+import History from './Containers/History/History.tsx';
 
 const App = () => <>
   <Toolbar/>
@@ -11,8 +14,11 @@ const App = () => <>
     <Routes>
       <Route path="/" element={<Artists/>}/>
       <Route path="/artists" element={<Artists/>}/>
-      <Route path="/albums/:artistId" element={<Albums />} />
-      <Route path="/tracks/:albumId" element={<Tracks />} />
+      <Route path="/albums/:artistId" element={<Albums/>}/>
+      <Route path="/tracks/:albumId" element={<Tracks/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/register" element={<RegisterPage/>}/>
+      <Route path="/history" element={<History/>}/>
       <Route path="*" element={<h3 className="text-center">Page not found</h3>}/>
     </Routes>
   </div>
