3531469654b02349fd71036cd6946f307569303c
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-20 02:16:10 +0600

    [HW#86] 1. Created delete request for user to log out 2. Changed the route path from '/' to '/register' 3. Adjusted the res.sends info

--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import {Error} from "mongoose";
 import User from "../models/User";
+import auth, {RequestWithUser} from '../middleware/auth';
 
 const usersRouter = express.Router();
 
-usersRouter.post('/', async (req, res, next) => {
+usersRouter.post('/register', async (req, res, next) => {
     try {
         const user = new User({
             username: req.body.username,
@@ -14,10 +15,10 @@ usersRouter.post('/', async (req, res, next) => {
         user.generateToken();
 
         await user.save();
-        res.send(user);
+        res.send({user, message: "Successfully registered"});
     } catch (error) {
         if (error instanceof Error.ValidationError) {
-            res.status(400).send({error});
+            res.status(400).send(error);
             return;
         }
         next(error);
@@ -29,27 +30,44 @@ usersRouter.post('/sessions', async (req, res, next) => {
         const user = await User.findOne({username: req.body.username});
 
         if (!user) {
-            res.status(400).send({error: "Username not found"});
+            res.status(400).send({error: 'Username not found'});
             return;
         }
 
         const isMatch = await user.comparePassword(req.body.password);
 
         if (!isMatch) {
-            res.status(400).send({error: "Password is wrong"});
+            res.status(400).send({error: 'Wrong password'});
             return;
         }
 
         user.generateToken();
         await user.save();
 
-        res.send({message: "Username and password are correct", username: user.username, user_id: user._id});
+        res.send({message: 'Username and password are correct', user});
+
     } catch (error) {
         if (error instanceof Error.ValidationError) {
-            res.status(400).send({error});
+            res.status(400).send(error);
             return;
         }
         next(error);
+    }
+});
+
+usersRouter.delete('/sessions', auth, async (req, res, next) => {
+    let reqWithAuth = req as RequestWithUser;
+    const userFromAuth = reqWithAuth.user;
+    try {
+        const user = await User.findOne({_id: userFromAuth._id});
+
+        if (user) {
+            user.generateToken();
+            await user.save();
+            res.send({message: 'Successfully logged out'});
+        }
+    } catch (e) {
+        next(e);
     }
 });
 
