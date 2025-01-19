b6da3453df73efebbaccffddd8eb9c680599be55
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-20 02:38:26 +0600

    [HW#86] Updated track component by adding a modal window to play music and send the info to server about the track user listened after clicking the button

--- frontend/src/Components/Track/Track.tsx
+++ frontend/src/Components/Track/Track.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
-import { ITrack } from '../../types';
+import React, { useState } from 'react';
+import { ITrack, IUser } from '../../types';
+import { useAppDispatch, useAppSelector } from '../../app/hooks.ts';
+import { selectUser } from '../../store/slices/usersSlice.ts';
+import { addTrackToHistory } from '../../store/thunks/trackHistoryThunk.ts';
+import Modal from '../UI/Modal/Modal.tsx';
 
 interface Props {
   track: ITrack;
 }
 
 const Track: React.FC<Props> = ({track}) => {
+  const user: IUser | null = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const onModalClick = (): void => {
+    setShowModal((prevState) => !prevState);
+  };
+
+  const playTrack = async () => {
+    try {
+      if (track.youtubeLink) {
+        onModalClick();
+      }
+      await dispatch(addTrackToHistory(track._id)).unwrap();
+    } catch (error) {
+      console.error('Failed to add track to history:', error);
+    }
+  };
+
   return (
-    <li className="list-group-item d-flex justify-content-between align-items-center">
-      <p className="m-0 p-0 fs-5"><span className="fw-bold">{track.track_number}.</span> {track.name}</p>
-      <p className="m-0 p-0 fw-bold">{track.duration}</p>
-    </li>
+    <>
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <p className="m-0 p-0 fs-5"><span className="fw-bold">{track.track_number}.</span> {track.name}</p>
+        <div className="d-flex justify-content-end gap-3 align-items-center">
+          <p className="m-0 p-0 fw-bold">{track.duration}</p>
+          {user ? <button className="btn btn-outline-dark" onClick={playTrack}><i className="bi bi-play-fill"></i>
+          </button> : null}
+        </div>
+      </li>
+
+      {track.youtubeLink && (
+        <Modal show={showModal} onModalClick={onModalClick} title={track.name}>
+          <iframe
+            width="100%"
+            height="315"
+            src={showModal ? `${track.youtubeLink}?autoplay=1` : undefined}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </Modal>
+      )}
+    </>
   );
 };
 
