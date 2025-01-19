e32a4a71077fcd62a076b883dca3d8e3547d1eeb
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-20 02:40:42 +0600

    [HW#86] Wrapped the app into PersistGate component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,17 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
-import { store } from './app/store.ts';
+import { persistor, store } from './app/store.ts';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <ToastContainer/>
-      <App/>
-    </Provider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <ToastContainer/>
+        <App/>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
 );
