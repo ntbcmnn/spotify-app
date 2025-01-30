43ae08025fd48c824ef2070261a89e592d6f9fa5
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-30 17:07:04 +0600

    [HW-89] Removed types for the essences, added role field into UserFields interface

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,25 +1,6 @@
-export interface IArtist {
-    name: string;
-    info: string;
-    image: string | null;
-}
-
-export interface IAlbum {
-    name: string;
-    artist: string;
-    release_year: number;
-    image: string | null;
-}
-
-export interface ITrack {
-    name: string;
-    album: string;
-    duration: string;
-    track_number: number;
-}
-
 export interface UserFields {
     username: string;
     password: string;
     token: string;
+    role: string;
 }
