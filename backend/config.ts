3c442f8f50d3bb1999da3c9c2e107ec329f984d8
Natalya Botsman <botsmannatashaa@gmail.com>
2025-02-04 18:09:22 +0600

    [HW-91] Added google object to config to correctly process data when user logs in with google

--- backend/config.ts
+++ backend/config.ts
@@ -1,12 +1,16 @@
 import path from "path";
 
 
-const rootPath  = __dirname;
+const rootPath = __dirname;
 
 const config = {
     rootPath,
     publicPath: path.join(rootPath, 'public'),
     db: 'mongodb://localhost/spotify',
+    google: {
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
 };
 
 export default config;
