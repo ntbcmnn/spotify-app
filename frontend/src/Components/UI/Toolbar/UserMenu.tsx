bcc17b08e4fa50deab7b53e9b547b1e5cdc2ae3e
Natalya Botsman <botsmannatashaa@gmail.com>
2025-02-04 18:20:18 +0600

    [HW-91] Updated the menu for user: 1. Added a validation to check if user.avatar contains 'images/' or 'fixtures/' in path name to correctly display avatars for google users and users, that registered usual way 2. Now if the mouse leaves the dropdown menu - it closes

--- frontend/src/Components/UI/Toolbar/UserMenu.tsx
+++ frontend/src/Components/UI/Toolbar/UserMenu.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch } from '../../../app/hooks';
 import { unsetUser } from '../../../store/slices/usersSlice';
 import { logout } from '../../../store/thunks/usersThunk';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { api_URL } from '../../../globalConstants.ts';
 
 interface Props {
   user: IUser;
@@ -34,15 +35,29 @@ const UserMenu: React.FC<Props> = ({user}) => {
         Track History
       </NavLink>
 
-      <div className="dropdown">
+      <div
+        className="dropdown"
+        onMouseLeave={toggleMenu}
+      >
         <button
           className="btn btn-outline-light border-0 dropdown-toggle d-inline-flex gap-2 align-items-center"
           type="button"
           onClick={toggleMenu}
         >
-          <i className="bi bi-person-circle"></i>
-          {user.username}
+          {
+            user.avatar ?
+              <img
+                className="rounded-circle user-avatar"
+                src={user.avatar.startsWith('images/') || user.avatar.startsWith('fixtures') ? `${api_URL}/${user.avatar}` : user.avatar}
+                alt={user.username}
+              />
+              :
+              <i className="bi bi-person-circle"></i>
+          }
+
+          {user.displayName}
         </button>
+
         <div className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
           <button className="dropdown-item btn btn-dark" onClick={handleLogOut}>Log out</button>
         </div>
