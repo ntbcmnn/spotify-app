adc4cd9af15aa475c432f799bd62eaecf28ce53d
Natalya Botsman <botsmannatashaa@gmail.com>
2025-02-04 18:13:42 +0600

    [HW-91] Updated routes: 1. Created route for google login 2. Updated fields in register route by adding displayName and avatar

--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -2,14 +2,66 @@ import express from "express";
 import {Error} from "mongoose";
 import User from "../models/User";
 import auth, {RequestWithUser} from '../middleware/auth';
+import {OAuth2Client} from 'google-auth-library';
+import config from '../config';
+import {imagesUpload} from '../multer';
 
 const usersRouter = express.Router();
+const client = new OAuth2Client(config.google.clientId);
 
-usersRouter.post('/register', async (req, res, next) => {
+usersRouter.post('/google', async (req, res, next) => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: req.body.credential,
+            audience: config.google.clientId,
+        });
+
+        const payload = ticket.getPayload();
+        console.log(payload);
+        if (!payload) {
+            res.status(400).send({error: 'Google login error'});
+            return;
+        }
+
+        const email = payload.email;
+        const id = payload.sub;
+        const displayName = payload.name;
+        const avatar = payload.picture;
+
+        if (!email) {
+            res.status(400).send({error: 'Not enough user data to continue'});
+            return;
+        }
+
+        let user = await User.findOne({googleID: id});
+
+        if (!user) {
+            user = new User({
+                username: email,
+                password: crypto.randomUUID(),
+                googleID: id,
+                displayName,
+                avatar
+            });
+        }
+
+        user.generateToken();
+        await user.save();
+
+        res.send({message: 'Login with Google went successfully', user});
+        return;
+    } catch (e) {
+        next(e);
+    }
+});
+
+usersRouter.post('/register', imagesUpload.single('avatar'), async (req, res, next) => {
     try {
         const user = new User({
             username: req.body.username,
             password: req.body.password,
+            displayName: req.body.displayName,
+            avatar: req.file ? 'images' + req.file.filename : null,
         });
 
         user.generateToken();
